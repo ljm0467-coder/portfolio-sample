f1d726478e8358dd5af910e6d81cea8fe4abb770
ljm0467-corder <ljm0467@gmail.com>
2025-09-19 15:42:07 +0900

    프로젝트 최초 생성

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+const swHero = new Swiper(".sw-hero", {
+ // slidesPerView: 3,
+ // spaceBetween: 30,
+ loop: true,
+ autoplay: {
+  delay: 2500,
+  disableOnInteraction: false,
+ },
+ pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+ },
+ navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+ },
+});
